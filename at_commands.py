3dc44843c7736c11a5e3016204b567a4eced99fa
Sergii Maksymov <s.maximov@proton.me>
2024-08-29 09:56:10 +0200

    update at_commands.py

--- at_commands.py
+++ at_commands.py
@@ -5,7 +5,8 @@ led3 = pyb.LED(3)
 
 uart = pyb.UART(1, 38400)
 
-command = "AT+VERSION?" + "\r\n"
+arg = "NAME?"
+command = "AT+" + arg + "\r\n"
 
 while True:
     uart.write(command)
@@ -15,3 +16,12 @@ while True:
         break
 
 
+'''
+pyboard - BT adapter
+Y9        RXD
+Y10       TXD
+GND       GND
+3V3       KEY
+V+        VCC
+
+'''
